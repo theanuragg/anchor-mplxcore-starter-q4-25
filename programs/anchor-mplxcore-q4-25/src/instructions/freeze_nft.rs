23f93cedf317ce16ddfd690ac3f9e3f0277e11b7
anurag <anurags9693@gmail.com>
2026-02-08 12:24:21 +0530

    add

---
+++ programs/anchor-mplxcore-q4-25/src/instructions/freeze_nft.rs
@@ -0,0 +1,58 @@
+use anchor_lang::prelude::*;
+use mpl_core::{
+    instructions::UpdatePluginV1CpiBuilder,
+    types::{FreezeDelegate, Plugin},
+    ID as CORE_PROGRAM_ID,
+};
+
+use crate::{error::MPLXCoreError, state::CollectionAuthority};
+
+#[derive(Accounts)]
+pub struct FreezeNft<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        mut,
+        constraint = !asset.data_is_empty() @ MPLXCoreError::AssetAlreadyInitialized
+    )]
+    /// CHECK: Checked by MPL Core
+    pub asset: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    /// CHECK: Checked by MPL Core
+    pub collection: UncheckedAccount<'info>,
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+        constraint = collection_authority.creator == authority.key() @ MPLXCoreError::NotAuthorized
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: MPL Core program
+    pub core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> FreezeNft<'info> {
+    pub fn freeze_nft(&mut self) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority",
+            &self.collection.key().to_bytes(),
+            &[self.collection_authority.bump],
+        ]];
+
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.authority.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: true }))
+            .invoke_signed(signer_seeds)?;
+
+        Ok(())
+    }
+}
---
+++ programs/anchor-mplxcore-q4-25/src/instructions/update_nft.rs
@@ -0,0 +1,54 @@
+use anchor_lang::prelude::*;
+use mpl_core::{instructions::UpdateV2CpiBuilder, ID as CORE_PROGRAM_ID};
+
+use crate::{error::MPLXCoreError, state::CollectionAuthority};
+
+#[derive(Accounts)]
+pub struct UpdateNft<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        mut,
+        constraint = !asset.data_is_empty() @ MPLXCoreError::AssetAlreadyInitialized
+    )]
+    /// CHECK: Checked by MPL Core
+    pub asset: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ MPLXCoreError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ MPLXCoreError::CollectionNotInitialized
+    )]
+    /// CHECK: Checked by MPL Core
+    pub collection: UncheckedAccount<'info>,
+    #[account(
+        seeds = [b"collection_authority", collection.key().as_ref()],
+        bump = collection_authority.bump,
+        constraint = collection_authority.creator == authority.key() @ MPLXCoreError::NotAuthorized
+    )]
+    pub collection_authority: Account<'info, CollectionAuthority>,
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: MPL Core program
+    pub core_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> UpdateNft<'info> {
+    pub fn update_nft(&mut self, new_name: String) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"collection_authority",
+            &self.collection.key().to_bytes(),
+            &[self.collection_authority.bump],
+        ]];
+
+        UpdateV2CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.authority.to_account_info())
+            .authority(Some(&self.collection_authority.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .new_name(new_name)
+            .invoke_signed(signer_seeds)?;
+
+        Ok(())
+    }
+}
